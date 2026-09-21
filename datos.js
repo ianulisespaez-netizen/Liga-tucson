b1e1763cabf7623618f56e64b26737889df80e8d
ianulisespaez-netizen <ianulisespaez@gmail.com>
2026-09-21 15:03:22 -0300

    Create datos.js

---
+++ datos.js
@@ -0,0 +1,802 @@
+// ==========================================
+// LIGA TUCSON
+// Primera División A - Clausura 2026
+// Datos correspondientes a Fecha 5
+// ==========================================
+
+const datosLiga = {
+
+    torneo: "Primera División A - Clausura 2026",
+    fechaActual: 5,
+
+    // ==========================================
+    // ZONAS Y TABLAS DE POSICIONES
+    // ==========================================
+
+    zonas: {
+
+        A: [
+            {
+                equipo: "Talleres",
+                pts: 10,
+                pj: 4,
+                pg: 3,
+                pe: 1,
+                pp: 0,
+                gf: 4,
+                gc: 1,
+                dg: 3
+            },
+            {
+                equipo: "Juventud Unida",
+                pts: 7,
+                pj: 5,
+                pg: 2,
+                pe: 1,
+                pp: 2,
+                gf: 6,
+                gc: 8,
+                dg: -2
+            },
+            {
+                equipo: "Estación Experimental",
+                pts: 6,
+                pj: 3,
+                pg: 2,
+                pe: 0,
+                pp: 1,
+                gf: 4,
+                gc: 3,
+                dg: 1
+            },
+            {
+                equipo: "Central Norte",
+                pts: 4,
+                pj: 4,
+                pg: 1,
+                pe: 1,
+                pp: 2,
+                gf: 3,
+                gc: 4,
+                dg: -1
+            },
+            {
+                equipo: "Villa Mitre",
+                pts: 3,
+                pj: 4,
+                pg: 1,
+                pe: 0,
+                pp: 3,
+                gf: 5,
+                gc: 6,
+                dg: -1
+            },
+            {
+                equipo: "All Boys",
+                pts: 3,
+                pj: 4,
+                pg: 1,
+                pe: 0,
+                pp: 3,
+                gf: 3,
+                gc: 7,
+                dg: -4
+            },
+            {
+                equipo: "San José",
+                pts: 3,
+                pj: 5,
+                pg: 1,
+                pe: 0,
+                pp: 4,
+                gf: 2,
+                gc: 7,
+                dg: -5
+            }
+        ],
+
+        B: [
+            {
+                equipo: "Argentinos del Norte",
+                pts: 9,
+                pj: 4,
+                pg: 3,
+                pe: 0,
+                pp: 1,
+                gf: 7,
+                gc: 4,
+                dg: 3
+            },
+            {
+                equipo: "San Martín",
+                pts: 8,
+                pj: 5,
+                pg: 2,
+                pe: 2,
+                pp: 1,
+                gf: 5,
+                gc: 5,
+                dg: 0
+            },
+            {
+                equipo: "San Antonio",
+                pts: 6,
+                pj: 4,
+                pg: 2,
+                pe: 0,
+                pp: 2,
+                gf: 5,
+                gc: 4,
+                dg: 1
+            },
+            {
+                equipo: "Atlético Tucumán",
+                pts: 5,
+                pj: 5,
+                pg: 1,
+                pe: 2,
+                pp: 2,
+                gf: 4,
+                gc: 4,
+                dg: 0
+            },
+            {
+                equipo: "Sportivo Guzmán",
+                pts: 4,
+                pj: 3,
+                pg: 1,
+                pe: 1,
+                pp: 1,
+                gf: 2,
+                gc: 2,
+                dg: 0
+            },
+            {
+                equipo: "Amalia",
+                pts: 4,
+                pj: 5,
+                pg: 1,
+                pe: 1,
+                pp: 3,
+                gf: 1,
+                gc: 5,
+                dg: -4
+            },
+            {
+                equipo: "Tucumán Central",
+                pts: 3,
+                pj: 4,
+                pg: 0,
+                pe: 3,
+                pp: 1,
+                gf: 3,
+                gc: 4,
+                dg: -1
+            }
+        ],
+
+        C: [
+            {
+                equipo: "Garmendia",
+                pts: 11,
+                pj: 5,
+                pg: 3,
+                pe: 2,
+                pp: 0,
+                gf: 9,
+                gc: 6,
+                dg: 3
+            },
+            {
+                equipo: "Atlético Concepción",
+                pts: 10,
+                pj: 5,
+                pg: 3,
+                pe: 1,
+                pp: 1,
+                gf: 9,
+                gc: 6,
+                dg: 3
+            },
+            {
+                equipo: "San Juan",
+                pts: 8,
+                pj: 4,
+                pg: 2,
+                pe: 2,
+                pp: 0,
+                gf: 5,
+                gc: 3,
+                dg: 2
+            },
+            {
+                equipo: "San Lorenzo (DG)",
+                pts: 7,
+                pj: 5,
+                pg: 2,
+                pe: 2,
+                pp: 1,
+                gf: 6,
+                gc: 5,
+                dg: 1
+            },
+            {
+                equipo: "Unión del Norte",
+                pts: 6,
+                pj: 5,
+                pg: 1,
+                pe: 3,
+                pp: 1,
+                gf: 5,
+                gc: 3,
+                dg: 2
+            },
+            {
+                equipo: "Lastenia",
+                pts: 5,
+                pj: 5,
+                pg: 1,
+                pe: 2,
+                pp: 2,
+                gf: 3,
+                gc: 4,
+                dg: -1
+            },
+            {
+                equipo: "Cruz Alta",
+                pts: 0,
+                pj: 5,
+                pg: 0,
+                pe: 0,
+                pp: 5,
+                gf: 2,
+                gc: 15,
+                dg: -13
+            }
+        ],
+
+        D: [
+            {
+                equipo: "Bella Vista",
+                pts: 10,
+                pj: 5,
+                pg: 3,
+                pe: 1,
+                pp: 1,
+                gf: 11,
+                gc: 4,
+                dg: 7
+            },
+            {
+                equipo: "San Fernando",
+                pts: 9,
+                pj: 5,
+                pg: 3,
+                pe: 0,
+                pp: 2,
+                gf: 8,
+                gc: 9,
+                dg: -1
+            },
+            {
+                equipo: "Almirante Brown",
+                pts: 8,
+                pj: 5,
+                pg: 2,
+                pe: 2,
+                pp: 1,
+                gf: 13,
+                gc: 6,
+                dg: 7
+            },
+            {
+                equipo: "San Pablo",
+                pts: 6,
+                pj: 3,
+                pg: 2,
+                pe: 0,
+                pp: 1,
+                gf: 5,
+                gc: 4,
+                dg: 1
+            },
+            {
+                equipo: "Famaillá",
+                pts: 6,
+                pj: 4,
+                pg: 2,
+                pe: 0,
+                pp: 2,
+                gf: 4,
+                gc: 5,
+                dg: -1
+            },
+            {
+                equipo: "Ateneo Parroquial Alderetes",
+                pts: 5,
+                pj: 4,
+                pg: 1,
+                pe: 2,
+                pp: 1,
+                gf: 7,
+                gc: 6,
+                dg: 1
+            },
+            {
+                equipo: "Santa Lucía",
+                pts: 5,
+                pj: 5,
+                pg: 1,
+                pe: 2,
+                pp: 2,
+                gf: 2,
+                gc: 7,
+                dg: -5
+            }
+        ],
+
+        E: [
+            {
+                equipo: "Alto Verde",
+                pts: 12,
+                pj: 5,
+                pg: 4,
+                pe: 0,
+                pp: 1,
+                gf: 14,
+                gc: 5,
+                dg: 9
+            },
+            {
+                equipo: "Concepción FC",
+                pts: 8,
+                pj: 4,
+                pg: 2,
+                pe: 2,
+                pp: 0,
+                gf: 5,
+                gc: 3,
+                dg: 2
+            },
+            {
+                equipo: "Azucarera Argentina",
+                pts: 7,
+                pj: 5,
+                pg: 2,
+                pe: 1,
+                pp: 2,
+                gf: 4,
+                gc: 10,
+                dg: -6
+            },
+            {
+                equipo: "Ñuñorco",
+                pts: 6,
+                pj: 5,
+                pg: 2,
+                pe: 0,
+                pp: 3,
+                gf: 6,
+                gc: 6,
+                dg: 0
+            },
+            {
+                equipo: "Unión Simoca",
+                pts: 6,
+                pj: 5,
+                pg: 1,
+                pe: 3,
+                pp: 1,
+                gf: 5,
+                gc: 5,
+                dg: 0
+            },
+            {
+                equipo: "San Ramón",
+                pts: 5,
+                pj: 4,
+                pg: 1,
+                pe: 2,
+                pp: 1,
+                gf: 4,
+                gc: 3,
+                dg: 1
+            },
+            {
+                equipo: "Santa Rosa",
+                pts: 4,
+                pj: 5,
+                pg: 1,
+                pe: 1,
+                pp: 3,
+                gf: 4,
+                gc: 5,
+                dg: -1
+            },
+            {
+                equipo: "La Providencia",
+                pts: 4,
+                pj: 5,
+                pg: 1,
+                pe: 1,
+                pp: 3,
+                gf: 2,
+                gc: 7,
+                dg: -5
+            }
+        ],
+
+        F: [
+            {
+                equipo: "Deportivo Aguilares",
+                pts: 16,
+                pj: 6,
+                pg: 5,
+                pe: 1,
+                pp: 0,
+                gf: 10,
+                gc: 4,
+                dg: 6
+            },
+            {
+                equipo: "San Lorenzo (SA)",
+                pts: 10,
+                pj: 5,
+                pg: 3,
+                pe: 1,
+                pp: 1,
+                gf: 6,
+                gc: 4,
+                dg: 2
+            },
+            {
+                equipo: "Jorge Newbery",
+                pts: 8,
+                pj: 5,
+                pg: 2,
+                pe: 2,
+                pp: 1,
+                gf: 5,
+                gc: 3,
+                dg: 2
+            },
+            {
+                equipo: "Graneros",
+                pts: 6,
+                pj: 4,
+                pg: 2,
+                pe: 0,
+                pp: 2,
+                gf: 6,
+                gc: 4,
+                dg: 2
+            },
+            {
+                equipo: "Santa Ana",
+                pts: 4,
+                pj: 5,
+                pg: 1,
+                pe: 1,
+                pp: 3,
+                gf: 5,
+                gc: 8,
+                dg: -3
+            },
+            {
+                equipo: "Trinidad",
+                pts: 4,
+                pj: 4,
+                pg: 1,
+                pe: 1,
+                pp: 2,
+                gf: 2,
+                gc: 3,
+                dg: -1
+            },
+            {
+                equipo: "Marapa",
+                pts: 4,
+                pj: 5,
+                pg: 1,
+                pe: 1,
+                pp: 3,
+                gf: 5,
+                gc: 12,
+                dg: -7
+            },
+            {
+                equipo: "Llorens",
+                pts: 3,
+                pj: 5,
+                pg: 1,
+                pe: 0,
+                pp: 4,
+                gf: 6,
+                gc: 12,
+                dg: -6
+            }
+        ]
+    },
+
+
+    // ==========================================
+    // PARTIDOS / RESULTADOS
+    // ==========================================
+
+    partidos: [
+
+        // ---------- ZONA A ----------
+        {
+            fecha: 5,
+            zona: "A",
+            local: "Juventud Unida",
+            visitante: "San José",
+            golesLocal: 3,
+            golesVisitante: 1,
+            estado: "finalizado"
+        },
+        {
+            fecha: 5,
+            zona: "A",
+            local: "Villa Mitre",
+            visitante: "Estación Experimental",
+            estado: "programado"
+        },
+        {
+            fecha: 5,
+            zona: "A",
+            local: "Talleres",
+            visitante: "Central Norte",
+            estado: "programado"
+        },
+        {
+            fecha: 5,
+            zona: "A",
+            local: "San Fernando",
+            visitante: "All Boys",
+            golesLocal: 3,
+            golesVisitante: 1,
+            estado: "finalizado",
+            interzonal: true
+        },
+
+
+        // ---------- ZONA B ----------
+        {
+            fecha: 5,
+            zona: "B",
+            local: "Amalia",
+            visitante: "Atlético Tucumán",
+            golesLocal: 0,
+            golesVisitante: 2,
+            estado: "finalizado"
+        },
+        {
+            fecha: 5,
+            zona: "B",
+            local: "Tucumán Central",
+            visitante: "Argentinos del Norte",
+            estado: "programado"
+        },
+        {
+            fecha: 5,
+            zona: "B",
+            local: "Sportivo Guzmán",
+            visitante: "San Antonio",
+            estado: "programado"
+        },
+        {
+            fecha: 5,
+            zona: "B",
+            local: "San Lorenzo (DG)",
+            visitante: "San Martín",
+            golesLocal: 1,
+            golesVisitante: 2,
+            estado: "finalizado",
+            interzonal: true
+        },
+
+
+        // ---------- ZONA C ----------
+        // La programación de Fecha 5 visible corresponde
+        // principalmente a partidos interzonales.
+
+
+        // ---------- ZONA D ----------
+        {
+            fecha: 5,
+            zona: "D",
+            local: "Almirante Brown",
+            visitante: "Bella Vista",
+            golesLocal: 1,
+            golesVisitante: 1,
+            estado: "finalizado"
+        },
+        {
+            fecha: 5,
+            zona: "D",
+            local: "Santa Lucía",
+            visitante: "Famaillá",
+            golesLocal: 0,
+            golesVisitante: 1,
+            estado: "finalizado"
+        },
+        {
+            fecha: 5,
+            zona: "D",
+            local: "San Pablo",
+            visitante: "Ateneo Parroquial Alderetes",
+            estado: "programado"
+        },
+        {
+            fecha: 5,
+            zona: "D",
+            local: "San Fernando",
+            visitante: "All Boys",
+            golesLocal: 3,
+            golesVisitante: 1,
+            estado: "finalizado",
+            interzonal: true
+        },
+
+
+        // ---------- ZONA E ----------
+        {
+            fecha: 5,
+            zona: "E",
+            local: "Alto Verde",
+            visitante: "Santa Rosa",
+            golesLocal: 2,
+            golesVisitante: 1,
+            estado: "finalizado"
+        },
+        {
+            fecha: 5,
+            zona: "E",
+            local: "Azucarera Argentina",
+            visitante: "Ñuñorco",
+            golesLocal: 2,
+            golesVisitante: 1,
+            estado: "finalizado"
+        },
+        {
+            fecha: 5,
+            zona: "E",
+            local: "La Providencia",
+            visitante: "Unión Simoca",
+            golesLocal: 0,
+            golesVisitante: 1,
+            estado: "finalizado"
+        },
+        {
+            fecha: 5,
+            zona: "E",
+            local: "Concepción FC",
+            visitante: "San Ramón",
+            estado: "programado"
+        },
+
+
+        // ---------- ZONA F ----------
+        {
+            fecha: 5,
+            zona: "F",
+            local: "Santa Ana",
+            visitante: "Llorens",
+            golesLocal: 4,
+            golesVisitante: 0,
+            estado: "finalizado"
+        },
+        {
+            fecha: 5,
+            zona: "F",
+            local: "San Lorenzo (SA)",
+            visitante: "Marapa",
+            golesLocal: 1,
+            golesVisitante: 0,
+            estado: "finalizado"
+        },
+        {
+            fecha: 5,
+            zona: "F",
+            local: "Deportivo Aguilares",
+            visitante: "Jorge Newbery",
+            golesLocal: 1,
+            golesVisitante: 0,
+            estado: "finalizado"
+        },
+        {
+            fecha: 5,
+            zona: "F",
+            local: "Graneros",
+            visitante: "Trinidad",
+            estado: "programado"
+        }
+    ]
+
+};
+
+
+// ==========================================
+// FUNCIONES AUXILIARES
+// ==========================================
+
+// Obtener todos los equipos de todas las zonas
+function obtenerTodosLosEquipos() {
+
+    const equipos = [];
+
+    Object.keys(datosLiga.zonas).forEach(zona => {
+
+        datosLiga.zonas[zona].forEach(equipo => {
+
+            equipos.push({
+                nombre: equipo.equipo,
+                zona: zona,
+                pts: equipo.pts,
+                pj: equipo.pj,
+                pg: equipo.pg,
+                pe: equipo.pe,
+                pp: equipo.pp,
+                gf: equipo.gf,
+                gc: equipo.gc,
+                dg: equipo.dg
+            });
+
+        });
+
+    });
+
+    return equipos;
+}
+
+
+// Buscar un equipo por nombre
+function buscarEquipo(nombre) {
+
+    const equipos = obtenerTodosLosEquipos();
+
+    return equipos.find(
+        equipo =>
+            equipo.nombre.toLowerCase() === nombre.toLowerCase()
+    );
+
+}
+
+
+// Obtener la tabla de una zona
+function obtenerTabla(zona) {
+
+    return datosLiga.zonas[zona] || [];
+
+}
+
+
+// Obtener partidos de una zona
+function obtenerPartidosZona(zona) {
+
+    return datosLiga.partidos.filter(
+        partido => partido.zona === zona
+    );
+
+}
+
+
+// Obtener partidos finalizados
+function obtenerResultados() {
+
+    return datosLiga.partidos.filter(
+        partido => partido.estado === "finalizado"
+    );
+
+}
+
+
+// Obtener próximos partidos
+function obtenerProximosPartidos() {
+
+    return datosLiga.partidos.filter(
+        partido => partido.estado === "programado"
+    );
+
+}
